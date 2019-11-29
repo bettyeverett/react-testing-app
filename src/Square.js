bebc18d516c2e766b32dd21e9e7de76e3723bf26
Bethany Everett <bethanyeeverett@gmail.com>
2019-11-29 10:25:13 +0000

    Updating square with few syntax changes.

--- src/Square.js
+++ src/Square.js
@@ -1,12 +1,11 @@
 // Update your <Square> component so that it can accept a colour prop, which will change the background colour of the square. Also add a default value for colour.
 import React from "react";
   
-const Square = ({ colour }) => (
+const Square = ( { colour } ) => (
   <div style={ { 
     backgroundColor: colour,
     width: "200px", 
     height: "200px" } }>
-
   </div>
 );
 
