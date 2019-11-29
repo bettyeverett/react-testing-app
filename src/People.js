304b6d6efa3d6fb9d18664d73daf509baf39ec67
Bethany Everett <bethanyeeverett@gmail.com>
2019-11-29 12:31:40 +0000

    Adding an event of clicked to the list of people.

--- src/People.js
+++ src/People.js
@@ -1,21 +1,37 @@
 // Create a component <People> that displays a list of names in a <ul>. The names should comes from an array.
 // Update your <People> component so that it can accept an array of names with a names prop. If no names are passed in it should display a paragraph saying: "Nothing to see here"
-import React from "react";
+import React, { Component } from "react";
 
-// your component here
-const People = ( { names } ) => (
-    names ? (
-        <ul>
-            { /* use map to output an <li> for each */ } 
-            { /* item in the array */ }
-            { names.map((value, index) => (
-                <li key={ index }>
-                    <p>{ value }</p>
-                </li> 
-            ))}
-        </ul>
-    ) : <p>Nothing to see here!</p>
+class People extends Component {
 
-);
+    handleClick() {
+        console.log("Hello, world!")
+    }
+
+    render() {
+
+        let { names } = this.props;
+
+        return (
+
+            names ? (
+                <ul
+                    onClick={ this.handleClick }
+                >
+                    { /* use map to output an <li> for each */ } 
+                    { /* item in the array */ }
+                    { names.map((value, index) => (
+                        <li key={ index }>
+                            <p>{ value }</p>
+                        </li> 
+                    ))}
+                </ul>
+            ) : <p>Nothing to see here!</p>
+
+        );
+
+    }
+
+}
 
 export default People;
