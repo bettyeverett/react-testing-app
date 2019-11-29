270abb5a37ed15908622221121ecebdf094129af
Bethany Everett <bethanyeeverett@gmail.com>
2019-11-29 12:05:51 +0000

    Changing Header component from a function to a class.

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 
+class Header extends Component {
+
+    render() {
+
+        let { children } = this.props;
+
+        return (
+            <header className="jumbotron">
+                <h1>{ children }</h1>
+            </header>
+        );
+    }
+
+}
 // we use className to add classes for Bootstrap styling
-const Header = ({ children }) => (
-    <header className="jumbotron">
-        <h1>{ children }</h1>
-    </header>
- );
 
 export default Header;
