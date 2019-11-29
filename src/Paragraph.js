c068e88f5bb7b854c82efbb2b825780662cc014b
Bethany Everett <bethanyeeverett@gmail.com>
2019-11-29 12:10:47 +0000

    Updating Paragraph component from function to class.

--- src/Paragraph.js
+++ src/Paragraph.js
@@ -2,11 +2,21 @@
 
 // Update your <Paragraph> component so that it can accept a message prop, which it will display between the <p> tags. If no message is provided it should just display "Hello, world".
 
-import React from "react";
+import React, { Component } from "react";
 
-const Paragraph = ({ children }) => (
-    <p>{ children }</p>
-);
+class Paragraph extends Component {
+
+    render() {
+
+        let { children } = this.props;
+
+        return (
+            <p>{ children }</p>
+        );
+
+    }
+
+}
 
 Paragraph.defaultProps = {
     children: "Something has gone wrong and there isn't any text available right now!"
