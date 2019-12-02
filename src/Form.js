cc74e013b55b3b756263e91c938e2baf43528c6c
Bethany Everett <bethanyeeverett@gmail.com>
2019-12-02 16:01:09 +0000

    Creating an input field that adds the inputted number both each to a <ul> and keeping a running total in a paragraph underneath.

--- src/Form.js
+++ src/Form.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import Length from "./form/Length";
 import List from "./form/List";
+import Adder from "./form/Adder";
 
 const Form = () => (
     <>
       <Length />
       <List />
+      <Adder />
     </>
   );
 
---
+++ src/form/Adder.js
@@ -0,0 +1,70 @@
+// Create an <Adder> component. It should have a number <input> field and an "Add" <button>. When the "Add" button is pressed the number should be added to a list of numbers on screen and the total of all the numbers in the list should be displayed below. Make sure you test the total is correct: try 1, 2, and 3 and check you get 6
+
+import React, { Component } from "react";
+
+class Adder extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            input: "",  
+            counter: 0,
+            list: [],
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+
+    }
+
+    handleChange(e) {
+        this.setState( { input: e.currentTarget.value } );
+    }
+
+    handleClick() {
+
+        let { input, counter, list } = this.state;
+
+        this.setState( { 
+            list: [...list, input],
+            counter: +counter + +input,
+            input: 0,
+        });
+
+
+    }
+
+    render() {
+
+        let { label, name } = this.props;
+        let { input, counter, list } = this.state;
+
+        return (
+
+            <div className="form-group jumbotron">
+                <label>{ label }</label>  
+                <input 
+                    onChange={ this.handleChange }
+                    value={ input }
+                    className="form-control"
+                    name={ name }
+                />
+                <button onClick={ this.handleClick } >Add Me!</button>
+                <ul>
+                    { list.map((value, index) => (
+                        <li key={ index }>
+                            <p>{ value }</p>
+                        </li> 
+                    ))}
+                </ul>
+                <p>{ counter }</p>
+            </div>
+
+        );
+
+    }
+
+}
+
+export default Adder;
