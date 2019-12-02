8e0509eeee2cb9e295f19c1f2525a0e7fb2e6978
Bethany Everett <bethanyeeverett@gmail.com>
2019-12-02 19:42:32 +0000

    Creating two input fields, one for temp in fahrenheit and one for temp in Celsius that upon changing, update the other field with the relevent value.

--- src/Form.js
+++ src/Form.js
@@ -3,12 +3,14 @@ import React from "react";
 import Length from "./form/Length";
 import List from "./form/List";
 import Adder from "./form/Adder";
+import TempConverter from "./form/TempConverter";
 
 const Form = () => (
     <>
       <Length />
       <List />
       <Adder />
+      <TempConverter />
     </>
   );
 
---
+++ src/form/TempConverter.js
@@ -0,0 +1,71 @@
+// Create a <TempConverter> component. It should have a degrees F and degrees C <input>. When you type a number into one <input> it should update the value in the other.
+
+import React, { Component } from "react";
+
+class TempConverter extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            farenheit: 0,
+            celsius: 0,
+        }
+
+        this.handleChangeFarenheit = this.handleChangeFarenheit.bind(this);
+        this.handleChangeCelsius = this.handleChangeCelsius.bind(this);
+
+    }
+
+    handleChangeFarenheit(e) {
+
+        let value = e.currentTarget.value;
+
+        this.setState( { 
+            farenheit: value,
+            celsius: (value - 32) / 1.8,
+        } );
+    }
+
+    handleChangeCelsius(e) {
+
+        let value = e.currentTarget.value;
+
+        this.setState( { 
+            celsius: value,
+            farenheit: (value * 1.8) + 32,
+        } );
+    }
+
+    render() {
+
+        let { farenheit, celsius } = this.state;
+
+        return (
+
+            <>
+                <div className="form-group jumbotron">
+                    <label>Farenheit</label>
+                    <input 
+                        className="form-control"
+                        onChange={ this.handleChangeFarenheit }
+                        value={ farenheit }
+                    />
+                </div>
+                <div className="form-group jumbotron">
+                    <label>Celsius</label>
+                    <input 
+                        className="form-control"
+                        onChange={ this.handleChangeCelsius }
+                        value={ celsius }
+                    />
+                </div>
+            </>
+
+        );
+
+    }
+
+}
+
+export default TempConverter;
